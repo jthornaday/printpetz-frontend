40b431e5a330c2f1136dda5771507241df5712fe
jthornaday <jakehornaday@gmail.com>
2026-08-30 09:49:30 -0700

    Validate pet and model names separately

--- src/lib/validations/modelTraining.ts
+++ src/lib/validations/modelTraining.ts
@@ -2,6 +2,7 @@ import { IModelTrainingRequest } from "@/types/modelTraining";
 import { array, object, ObjectSchema, string } from "yup";
 
 export const modelTrainingSchema: ObjectSchema<IModelTrainingRequest> = object({
-  name: string().required("Name is required"),
+  petName: string().trim().required("Your pet's name is required"),
+  name: string().trim().required("A name for this pet model is required"),
   images: array(string().required()).required("Images are required"),
 });
