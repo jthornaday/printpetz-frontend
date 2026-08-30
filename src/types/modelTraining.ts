7f9656bd366f41e30431bd1936942ed718cce40d
jthornaday <jakehornaday@gmail.com>
2026-08-30 09:49:21 -0700

    Add pet name to model training request

--- src/types/modelTraining.ts
+++ src/types/modelTraining.ts
@@ -1,9 +1,11 @@
 export interface IModelTrainingRequest {
   name: string;
+  petName: string;
   images: string[];
 }
 
 export interface TrainModelRequest {
   name: string;
+  petName: string;
   images: string[];
 }
